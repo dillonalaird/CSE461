faaa5a71c56d6d8a245b2dd585d0c82a4c496168
cgt2 <codythomas2013@gmail.com>
2013-11-02 13:55:33 -0700

    StageA working

--- project1/JavaSockets.java
+++ project1/JavaSockets.java
@@ -1,28 +1,53 @@
 import java.io.*;
 import java.net.*;
-import java.nio.ByteBuffer;
-import java.nio.CharBuffer;
-import java.nio.charset.CharsetEncoder;
+import java.nio.*;
 
 public class JavaSockets {
+    public static final String HOST = "bicycle.cs.washington.edu";
 
-  public static void main(String[] args) {
-  }
 
-  public static void stageA() throws Exception {
-    DatagramSocket clientSocket = new DatagramSocket();
-    InetAddress IPAddress = InetAddress.getByName("");
-    String msg = "hello world";
-    CharsetEncoder ce = null;
+    public static void main(String[] args) throws Exception {
+	stageA("hello world");
+    }
+
+    public static void sendUDP(ByteBuffer sendData) throws Exception {
+	DatagramSocket clientSocket = new DatagramSocket();
+	InetAddress IPAddress = InetAddress.getByName(HOST);
+	DatagramPacket sendPacket = new DatagramPacket(sendData.array(), sendData.array().length, IPAddress, 12235);
+	clientSocket.send(sendPacket);
+	byte[] receiveData = new byte[100];
+	DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+	clientSocket.receive(receivePacket);
+	bytesToHex(receiveData);
+    }
+
+    public static void stageA(String message) throws Exception {
+	ByteBuffer sendData = ByteBuffer.allocate(24);
+	sendData.putInt(message.length() + 1); // payload_len
+	sendData.putInt(0);            // psecret
+	sendData.putShort((short) 1);  // step
+	sendData.putShort((short) 219);// student number
+	sendData.put(message.getBytes());
+	sendUDP(sendData);
+    }
     
-    ByteBuffer sendData = ByteBuffer.allocate(24);
-    sendData.putInt(12);           // payload_len
-    sendData.putInt(0);            // psecret
-    sendData.putShort((short) 1);  // step
-    sendData.putShort((short) 219);// student number
-    sendData.put(ce.encode(CharBuffer.wrap("hello world")));
-
-    //DatagramPacket sendPacket = new DatagramPacket(sendData, 
-    //		sendData.length, IPAddress, 12235);
-  }
+    public static void bytesToHex(byte[] bytes) throws Exception {
+	char[] hexArray = "0123456789ABCDEF".toCharArray();
+	char[] hexChars = new char[bytes.length * 2];
+	int v;
+	for ( int j = 0; j < bytes.length; j++ ) {
+	    v = bytes[j] & 0xFF;
+	    hexChars[j * 2] = hexArray[v >>> 4];
+	    hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+	}
+	String str = new String(hexChars);
+	for(int i = 0; i < str.length(); i+=2) {
+	    System.out.print(str.charAt(i));
+	    if(i + 1 < str.length()) {
+		System.out.print(str.charAt(i + 1));
+	    }
+	    System.out.print(" ");
+	}
+	System.out.println();
+    }
 }
