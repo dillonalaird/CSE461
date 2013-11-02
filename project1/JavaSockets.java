0012c05c828e272f81e50d1ef3631931bf734c17
cgt2 <codythomas2013@gmail.com>
2013-11-02 15:35:15 -0700

    Part a of b working maybe

--- project1/JavaSockets.java
+++ project1/JavaSockets.java
@@ -5,31 +5,95 @@ import java.nio.*;
 public class JavaSockets {
     public static final String HOST = "bicycle.cs.washington.edu";
 
-
     public static void main(String[] args) throws Exception {
-	stageA("hello world");
+	byte[] stageAres = stageA();
+	//	bytesToHex(stageAres);
+	stageB(stageAres);
     }
 
-    public static void sendUDP(ByteBuffer sendData) throws Exception {
+    public static byte[] receiveUDP(int port) throws Exception {
 	DatagramSocket clientSocket = new DatagramSocket();
 	InetAddress IPAddress = InetAddress.getByName(HOST);
-	DatagramPacket sendPacket = new DatagramPacket(sendData.array(), sendData.array().length, IPAddress, 12235);
-	clientSocket.send(sendPacket);
 	byte[] receiveData = new byte[100];
-	DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-	clientSocket.receive(receivePacket);
-	bytesToHex(receiveData);
+	DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length, IPAddress, port);
+	while(true) {
+	    try{
+		clientSocket.receive(receivePacket);
+		return receiveData;
+	    } catch(Exception e) {
+		System.out.println(e.toString());
+	    }
+	}
     }
 
-    public static void stageA(String message) throws Exception {
+    public static initializeSocket(int port) {
+	InetAddress IPAddress = InetAddress.getByName(HOST);
+	clientSocket = new DatagramSocket();
+	clientSocket.connect(IPAddress, port);
+	clientSocket.setSoTimeout(1000);
+    }
+
+    public static byte[] sendUDP(ByteBuffer sendData, int port) throws Exception {
+	InetAddress IPAddress = InetAddress.getByName(HOST);
+	DatagramSocket clientSocket = new DatagramSocket();
+	clientSocket.setSoTimeout(1000);
+	DatagramPacket sendPacket = new DatagramPacket(sendData.array(), sendData.array().length, IPAddress, port);
+	while(true) {
+	    try {
+		clientSocket.send(sendPacket);
+		byte[] receiveData = new byte[100];
+		DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+		clientSocket.receive(receivePacket);
+		return receiveData;
+	    } catch(Exception e) {
+		System.out.println(e.toString());
+	    }
+	}
+    }
+
+    public static void stageB(byte[] input) throws Exception {
+	ByteBuffer results = ByteBuffer.allocate(100);
+	results.put(input);
+	int num = results.getInt(12);
+	int len = results.getInt(16);
+	int port = results.getInt(20);
+	int psecret = results.getInt(24);
+	System.out.println("Num: " + num);
+	System.out.println("Len: " + len);
+	System.out.println("Port: " + port);
+	System.out.println("Secret: " + psecret);
+	for(int i = 0; i < num; i++) {
+	    int alloc = fourByteAlign(len + 4);
+	    ByteBuffer sendData = ByteBuffer.allocate(alloc + 12);
+	    sendData.putInt(len + 4); // payload_len
+	    sendData.putInt(psecret);            // psecret
+	    sendData.putShort((short) 1);  // step
+	    sendData.putShort((short) 219);// student number
+	    sendData.putInt(i);
+	    // bytesToHex(sendData.array());
+	    System.out.println("Packet " + i + " response: ");
+	    byte[] res = sendUDP(sendData, port);
+	    bytesToHex(res);
+	}
+	receiveUDP(port);
+    }
+
+    public static int fourByteAlign(int num) {
+	return ((num + 3) / 4) * 4;
+    }
+
+    public static byte[] stageA() throws Exception {
+	String message = "hello world";
 	ByteBuffer sendData = ByteBuffer.allocate(24);
 	sendData.putInt(message.length() + 1); // payload_len
 	sendData.putInt(0);            // psecret
 	sendData.putShort((short) 1);  // step
 	sendData.putShort((short) 219);// student number
 	sendData.put(message.getBytes());
-	sendUDP(sendData);
+	return sendUDP(sendData, 12235);
     }
+
+
     
     public static void bytesToHex(byte[] bytes) throws Exception {
 	char[] hexArray = "0123456789ABCDEF".toCharArray();
