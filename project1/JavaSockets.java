7d642020406fb3d3443e8a03b64377c7536554e8
Dillon Laird <dillonlaird@d-108-179-184-177.dhcp4.washington.edu>
2013-11-02 13:05:49 -0700

    started some stuff

--- project1/JavaSockets.java
+++ project1/JavaSockets.java
@@ -1,1 +1,28 @@
-// TEST
+import java.io.*;
+import java.net.*;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.CharsetEncoder;
+
+public class JavaSockets {
+
+  public static void main(String[] args) {
+  }
+
+  public static void stageA() throws Exception {
+    DatagramSocket clientSocket = new DatagramSocket();
+    InetAddress IPAddress = InetAddress.getByName("");
+    String msg = "hello world";
+    CharsetEncoder ce = null;
+    
+    ByteBuffer sendData = ByteBuffer.allocate(24);
+    sendData.putInt(12);           // payload_len
+    sendData.putInt(0);            // psecret
+    sendData.putShort((short) 1);  // step
+    sendData.putShort((short) 219);// student number
+    sendData.put(ce.encode(CharBuffer.wrap("hello world")));
+
+    //DatagramPacket sendPacket = new DatagramPacket(sendData, 
+    //		sendData.length, IPAddress, 12235);
+  }
+}
