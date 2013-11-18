e9158975c4d22b04942fe2eb722d9b932a011480
aeding <aeding@gmail.com>
2013-11-18 11:33:19 -0800

    Stage A now works

--- project2/Server.java
+++ project2/Server.java
@@ -57,8 +57,7 @@ public class Server {
 
       ipAddress = dPacket.getAddress();
       Packet461 p = new Packet461(ByteBuffer.wrap(dPacket.getData()));
-
-      if ("hello world".equals(new String(p.payload))
+      if ("hello world".equals(new String(p.payload).substring(0,11))
             && pSecret == p.secret)
       {
         sid = p.sid;
@@ -73,7 +72,7 @@ public class Server {
         buf.putInt(port);
         buf.putInt(secretA);
 
-        sendUDP(generatePacket((short) 1, buf.array()), START_PORT);
+        sendUDP(generatePacket((short) 1, buf.array()), dPacket.getPort());
         return true;
       }
       return false;
@@ -93,13 +92,20 @@ public class Server {
       buf.put(payload);
       return buf.array();
     }
-
+    
     public void sendUDP(byte[] sendData, int port) {
-      try {
-        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ipAddress, port);
-        dSocket.send(sendPacket);
-      } catch (Exception e) {
-        e.printStackTrace();
+      DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ipAddress, port);
+      bytesToHex(sendData);
+      System.out.println("port = " +  port);
+      System.out.println("sendPacket addr. = " + sendPacket.getAddress());
+      for (;;) {
+        try {
+          dSocket.send(sendPacket);
+          dSocket.receive(dPacket);
+          return;
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
       }
     }
 
@@ -125,4 +131,23 @@ public class Server {
     return ((num + 3) / 4) * 4;
   }
 
+  public static void bytesToHex(byte[] bytes) {
+    char[] hexArray = "0123456789ABCDEF".toCharArray();
+    char[] hexChars = new char[bytes.length * 2];
+    int v;
+    for ( int j = 0; j < bytes.length; j++ ) {
+        v = bytes[j] & 0xFF;
+        hexChars[j * 2] = hexArray[v >>> 4];
+        hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+    }
+    String str = new String(hexChars);
+    for(int i = 0; i < str.length(); i+=2) {
+        System.out.print(str.charAt(i));
+        if(i + 1 < str.length()) {
+      System.out.print(str.charAt(i + 1));
+        }
+        System.out.print(" ");
+    }
+    System.out.println();
+  }
 }
